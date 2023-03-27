8a28643b5d000f16bc7f09b2577b1d162435e15f
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-03-27 16:35:40 -0500

    complete some lessons in section 6 and do some research into getter and setter shorthand and properties

--- Program.cs
+++ Program.cs
@@ -16,18 +16,18 @@
         Product.TotalNoProducts++;
 
         //initialize fields
-        product1.productId = 1001;
-        product1.productName = "Mobile";
-        product1.cost = 20000;
-        product1.quantitiyInStock = 1200;
-        product2.productId = 1002;
-        product2.productName = "Laptop";
-        product2.cost = 45000;
-        product2.quantitiyInStock = 3400;
-        product3.productId = 1003;
-        product3.productName = "Speakers";
-        product3.cost = 36000;
-        product3.quantitiyInStock = 800;
+        product1.SetProductId(1001);
+        product1.SetProductName("Mobile");
+        product1.SetCost(20000);
+        product1.SetQuantityInStock(1200);
+        product2.SetProductId(1002);
+        product2.SetProductName("Laptop");
+        product2.SetCost(45000);
+        product2.SetQuantityInStock(3400);
+        product3.SetProductId(1003);
+        product3.SetProductName("Speakers");
+        product3.SetCost(36000);
+        product3.SetQuantityInStock(800);
 
         //Call method
 
@@ -40,30 +40,30 @@
         Console.WriteLine("Created by: " + developerName);
 
         Console.WriteLine("\nProduct 1:");
-        Console.WriteLine("Product ID: " + product1.productId);
-        Console.WriteLine("Product Name: " + product1.productName);
-        Console.WriteLine("Product Cost: " + product1.cost);
-        Console.WriteLine("Product Tax: " + product1.tax);
-        Console.WriteLine("Product Quantity: " + product1.quantitiyInStock);
-        Console.WriteLine("Date of Purchase: " + product1.dateOfPurchase);
+        Console.WriteLine("Product ID: " + product1.GetProductId());
+        Console.WriteLine("Product Name: " + product1.GetProductName());
+        Console.WriteLine("Product Cost: " + product1.GetCost());
+        Console.WriteLine("Product Tax: " + product1.GetTax());
+        Console.WriteLine("Product Quantity: " + product1.GetQuantityInStock());
+        Console.WriteLine("Date of Purchase: " + product1.GetDateOfPurchase());
 
         Console.WriteLine("\nProduct 2:");
-        Console.WriteLine("Product ID: " + product2.productId);
-        Console.WriteLine("Product Name: " + product2.productName);
-        Console.WriteLine("Product Cost: " + product2.cost);
-        Console.WriteLine("Product Tax: " + product2.tax);
-        Console.WriteLine("Product Quantity: " + product2.quantitiyInStock);
-        Console.WriteLine("Date of Purchase: " + product2.dateOfPurchase);
+        Console.WriteLine("Product ID: " + product2.GetProductId());
+        Console.WriteLine("Product Name: " + product2.GetProductName());
+        Console.WriteLine("Product Cost: " + product2.GetCost());
+        Console.WriteLine("Product Tax: " + product2.GetTax());
+        Console.WriteLine("Product Quantity: " + product2.GetQuantityInStock());
+        Console.WriteLine("Date of Purchase: " + product2.GetDateOfPurchase());
 
         Console.WriteLine("\nProduct 3:");
-        Console.WriteLine("Product ID: " + product3.productId);
-        Console.WriteLine("Product Name: " + product3.productName);
-        Console.WriteLine("Product Cost: " + product3.cost);
-        Console.WriteLine("Product Tax: " + product3.tax);
-        Console.WriteLine("Product Quantity: " + product3.quantitiyInStock);
-        Console.WriteLine("Date of Purchase: " + product3.dateOfPurchase);
+        Console.WriteLine("Product ID: " + product3.GetProductId());
+        Console.WriteLine("Product Name: " + product3.GetProductName());
+        Console.WriteLine("Product Cost: " + product3.GetCost());
+        Console.WriteLine("Product Tax: " + product3.GetTax());
+        Console.WriteLine("Product Quantity: " + product3.GetQuantityInStock());
+        Console.WriteLine("Date of Purchase: " + product3.GetDateOfPurchase());
 
-        int totalQuantity = product1.quantitiyInStock + product2.quantitiyInStock + product3.quantitiyInStock;
+        int totalQuantity = product1.GetQuantityInStock() + product2.GetQuantityInStock() + product3.GetQuantityInStock();
 
         Console.WriteLine("\nTotal Quantity: " + totalQuantity);
 
